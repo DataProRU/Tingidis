c9f03a74826016100886a45b30bd390e16fb9351
Pavel Ershov <ershov@Pavels-MacBook-Air.local>
2024-12-05 21:45:11 +0200

    first

---
+++ database.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Request, Depends
+from sqlalchemy import Column, Integer, String, Date, Text, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+# Строка подключения к базе данных
+DATABASE_URL = ""
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+
+# Создаем базовый класс для моделей
+Base = declarative_base()
+
+# Определение модели WebUser с использованием SQLAlchemy
+class WebUser(Base):
+    __tablename__ = "web_users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=False, server_default="user")
+
+
+class Users(Base):
+    __tablename__ = "tg_users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(255))
+    tg_username = Column(String(255))
+
+
+# Асинхронная функция для создания таблиц
+async def init_db():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def get_db():
+    async with async_session() as session:
+        yield session
+
+# Example route to test the database connection
+@router.get("/")
+async def read_root(db: AsyncSession = Depends(get_db)):
+    return {"message": "Database connection is working"}
---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from routes import users, auth_routes
+from database import init_db
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(auth_routes.router)
+app.include_router(users.router)
+
+
+
+
+@app.on_event("startup")
+async def startup():
+    # При старте приложения создаем таблицы
+    await init_db()
+
+@app.on_event("shutdown")
+async def shutdown():
+    # Отключаемся от базы данных при завершении работы
+    await database.disconnect()
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
